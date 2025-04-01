a50da4f7a6fe46d416c8cc831871e36ffaca24fe
Quintelajonas <jonasquintela.s@outlook.com>
2025-04-01 12:08:17 -0300

    Adicionando tecnologias - informativo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,19 @@ export default function Home() {
     <h1>Conhecça as Naves do Universo de Star Wars</h1>
 
     <p>
+      Projeto desenvolvido durante o treinamento de Next.js da OneBitCode
+    </p>
+
+    <p>Tecnologias Utilizadas</p>
+    <li>Next.js</li>
+    <li>TypeScript</li>
+    <li>Vercel</li>
+
+
+
+
+    <p>
+
       <Link  className="btn" href={"/"}>Ver todas</Link>
     </p>
    </main>
