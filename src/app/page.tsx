8b12b8b6467d641661edd47f456f590929dc8902
Quintelajonas <jonasquintela.s@outlook.com>
2025-04-01 11:50:50 -0300

    Aplicação finalizada

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+
+   <main>
+    <h1>Conhecça as Naves do Universo de Star Wars</h1>
+
+    <p>
+      <Link  className="btn" href={"/"}>Ver todas</Link>
+    </p>
+   </main>
+  );
+}
